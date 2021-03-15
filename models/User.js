4256b7802d8a8d60f70c20535573cfcbce65757d
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-03-15 16:45:57 -0700

    2.0

--- models/User.js
+++ models/User.js
@@ -1,81 +1,63 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
-// create our user model
+// create our User model
 class User extends Model {
-    // set up method to run on instance data (per user) to check password
-    checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
-    }
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
 }
 
-// define table columns and configuration
+// create fields/columns for User model
 User.init(
-    {
-        // TABLE COLUMN DEFINITIONS GO HERE
-        // define an id column
-        id: {
-            // use the special Sequelize DataTypes object to provide what type of data it is
-            type: DataTypes.INTEGER,
-            // this is the equivalent of SQL's 'NOT NULL' option
-            allowNull: false,
-            // instruct that this is the PRIMARY KEY
-            primaryKey: true,
-            //  turn on autoincrement
-            autoIncrement: true
-        },
-        // define a username column
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        // define email column 
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            // there cannot be a duplicate email values in this table
-            unique: true,
-            // if allowNull is set to false, we can run our data through validation
-            validate: {
-                isEmail: true
-            }
-        },
-        // define a password column
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                // min length of pass
-                len: [4]
-            }
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
-    {
-        hooks: {
-            // set up beforeCreate lifecycle 'hook' functionilty
-            async beforeCreate(newUserData) {
-                newUserData.password = await bcrypt.hash(newUserData.password, 10);
-                return newUserData;
-            },
-            // set up beforeUpdate lifecycle "hook" functionality
-            async beforeUpdate(updatedUserData) {
-                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-                return updatedUserData;
-            }
-        },
-        // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-        // pass in our imported sequelize connection (the direct connection to our database 
-        sequelize,
-        // dont automatically create createdAt/updatedAt timestamp fields
-        timestamps: false,
-        // dont pluralize name of database table
-        freezeTableName: true,
-        // use underscores instead of camel-casing 
-        underscored: true,
-        // make it so our model name stays lowercase in database
-        modelName: 'user'
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
     }
+  },
+  {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
 );
 
 module.exports = User;
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 
@@ -8,6 +12,10 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.set('views', __dirname + '/views'); // general config
+// app.set('view engine', 'html');
 
 // turn on routes
 app.use(routes);
